1b4d22d2c25377fe7c60b6064ace77410b491423
metromancn <metromancn@gmail.com>
2026-06-14 20:13:03 +0900

    First creation

---
+++ DeckTheseus/DeckTheseus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeckTheseus
+//
+//  Created by Nina Morimoto on 2026/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeckTheseus/DeckTheseus/DeckTheseusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckTheseusApp.swift
+//  DeckTheseus
+//
+//  Created by Nina Morimoto on 2026/06/14.
+//
+
+import SwiftUI
+
+@main
+struct DeckTheseusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
